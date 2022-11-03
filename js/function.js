e54795657812b8a01b7dea99c74d2384ad3ec813
Felipe Canales Cayuqueo <felipecanalescayuqueo@gmail.com>
2022-11-03 00:57:37 -0300

    Update function.js

--- js/function.js
+++ js/function.js
@@ -1,27 +1,8 @@
-function addData(label, data) {
-     myChart.data.dataset.splice(0,0,data);
+function addData(chart, label, data) {
+    chart.data.labels.push(label);
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.push(data);
+    });
 
-     myChart.update();
- }
-
- function updateChart(chart, dataObj) {
-     // Store data into the database
-     Object.assign(data, dataObj);
-
-     // Fetch data from the database and replace old data
-     chart.data.labels = Object.keys(data);
-     chart.data.datasets.forEach((dataset) => {
-         dataset.data = Object.values(data);
-     });
-
-     chart.update();
- }
-
-function Points(){
-
-    var cont, puntos = 0;
-
-    
-
-
-} 
+    chart.update();
+}
